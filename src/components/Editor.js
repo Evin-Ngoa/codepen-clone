e3cd4c69f160b4fcffce8f011d3c445984c73638
Evin-Ngoa <toneevin07@gmail.com>
2022-05-10 05:47:53 +0300

    fontawesome usage in collapse button on editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,23 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
 import { Controlled as ControlledEditor } from "react-codemirror2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCompressAlt, faExpandAlt } from "@fortawesome/free-solid-svg-icons";
 
-const Editor = ( props ) => {
+const Editor = (props) => {
   const { renderName, onChange, value, language } = props;
+  const [openEditor, setOpenEditor] = useState(true);
 
   const handleChange = (editor, data, value) => {
-    onChange(value)
-  }
+    onChange(value);
+  };
 
   return (
-    <div className="editor-wrapper">
+    <div className={`editor-wrapper ${openEditor ? "" : "collapse"}`}>
       <div className="title">
         {renderName}
-        <button>O/C</button>
+        <button
+          type="button"
+          className="resize-btn"
+          onClick={() => setOpenEditor((prevOpenEditor) => !prevOpenEditor)}
+        >
+          <FontAwesomeIcon icon={openEditor ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
@@ -27,7 +36,7 @@ const Editor = ( props ) => {
           lineWrapping: true,
           lint: true,
           mode: language,
-          theme: 'material',
+          theme: "material",
         }}
       />
     </div>
